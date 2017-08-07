2edb362ca51b3cbfe49d4a495dcc99341f31d936
Mike Slinn <mslinn@micronauticsresearch.com>
2017-08-06 20:25:06 -0700

    Moved classes around

--- src/main/scala/GitStats.scala
+++ src/main/scala/GitStats.scala
@@ -2,28 +2,37 @@ import com.micronautics.gitStats._
 
 object GitStats extends App with GitStatsOptionParsing {
   parser.parse(args, ConfigGitStats()) match {
-    case Some(config) => processAllRepos(config)
+    case Some(config) => new AllRepos(config).process
 
     case None => // arguments are bad, error message will have been displayed
   }
+}
 
-  private def processAllRepos(config: ConfigGitStats) = {
-    val commits: List[Commit] = for {
-      file <- gitProjectsUnder(config.directory)
-    } yield try {
-      new Repo(config, file).process
-    } catch {
-      case e: Throwable =>
-        Console.err.println("Error: " + e.getMessage + " git repo ignored")
-        Commit.zero
+class AllRepos(config: ConfigGitStats) {
+  def process: Commit = {
+    val repos: List[Repo] =
+      for {
+        file <- gitProjectsUnder(config.directory)
+      } yield new Repo(config, file)
+
+    val repoSubtotals = repos.map { repo =>
+      try {
+        repo.process
+      } catch {
+        case e: Throwable =>
+          Console.err.println("Error: " + e.getMessage + " git repo ignored")
+          Commit.zero
+      }
     }
 
-    val total: Commit = commits.fold(Commit.zero) {
+    val total: Commit = repoSubtotals.fold(Commit.zero) {
       case (acc, elem) => Commit(acc.added + elem.added, acc.deleted + elem.deleted)
     }
     println()
+
     val summary = total.summarize(config.authorFullName, config.gitRepoName, finalTotal=true)
     println(summary)
+
     total
   }
 }
