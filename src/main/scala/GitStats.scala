e809f1e3ddebbfc65458c08849f2923dc2ccec36
Mike Slinn <mslinn@micronauticsresearch.com>
2017-08-11 10:45:01 -0700

    Showing more information about timeperiod in summary

--- src/main/scala/GitStats.scala
+++ src/main/scala/GitStats.scala
@@ -1,4 +1,5 @@
 import com.micronautics.gitStats._
+import org.joda.time.{DateTime, Days}
 
 object GitStats extends App with GitStatsOptionParsing {
   parser.parse(args, ConfigGitStats()) match {
@@ -38,9 +39,20 @@ protected class AllRepos()(implicit config: ConfigGitStats) {
   }
 
   private def report(commitsByLanguageByRepo: List[(Repo, Commits)]) = {
+    val between: String = (
+      for {
+        from <- config.dateFrom
+        to   =  config.dateTo.getOrElse(DateTime.now.withTimeAtStartOfDay)
+      } yield {
+        val days = Days.daysBetween(from, to).getDays
+        s"for the $days days "
+      }
+    ).getOrElse("")
+
     val dateRange = s"${ config.fromFormatted.map(x => s"from $x").mkString } ${ config.toFormatted.map(x => s"to $x").mkString }".trim
-    val dateStr = if (dateRange.nonEmpty) dateRange else "for all time"
-    println(s"Summary of commits in ${ commitsByLanguageByRepo.size } projects $dateStr")
+    val dateStr = between + (if (dateRange.nonEmpty) dateRange else "for all time")
+
+    println(s"Summary of commits in ${ commitsByLanguageByRepo.size } project${ if (commitsByLanguageByRepo.size>1) "s" else "" } $dateStr")
 
     if (config.subtotals) commitsByLanguageByRepo.foreach {
       case (repo, commits) =>
@@ -56,8 +68,9 @@ protected class AllRepos()(implicit config: ConfigGitStats) {
         Commits(Nil)
           .combine(commitsByLanguageByRepo.map(_._2))
 
-      if (grandTotal.value.isEmpty) "No activity." else {
-        val projects = if (commitsByLanguageByRepo.size > 1) s" (lines changed across ${ commitsByLanguageByRepo.size } projects)" else ""
+      if (grandTotal.value.isEmpty) s"No activity across ${ commitsByLanguageByRepo.size } projects." else {
+        val projects = if (commitsByLanguageByRepo.size > 1) s" (lines changed across ${ commitsByLanguageByRepo.size } projects$between)" else ""
+
         if (config.excelWorkbook.isDefined)
           config.excelWorkbook.foreach(_.addSheet(title = s"Subtotals By Language$projects", Commit.zero, contents = grandTotal.value))
         else
