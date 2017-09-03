b1f7711ad71e67fbd4ace9c0ffb2e11ae3d59381
Arseniy Tashoyan <tashoyan@gmail.com>
2017-09-04 00:41:04 +0300

    Support only numerical version components - a lot easier to compare. Looks enough for Subversion and Git.

--- src/main/scala/com/micronautics/gitStats/Version.scala
+++ src/main/scala/com/micronautics/gitStats/Version.scala
@@ -5,13 +5,12 @@ import com.micronautics.gitStats.Version._
 import scala.math.signum
 
 /**
-  * Multi-component version, like 1.2.3
-  * @param major Major version component, mandatory and non-empty.
+  * Multi-component numerical version, like 1.2.3.
+  *
+  * @param major  Major version component, mandatory.
   * @param minors Minor version components, may be empty.
   */
-case class Version(major: String, minors: String*) extends Ordered[Version] {
-  require(major != null, "Major version must not be null")
-  require(!major.isEmpty, "Major version must not be empty string")
+case class Version(major: Int, minors: Int*) extends Ordered[Version] {
 
   private val minorsList = minors.toList
 
@@ -27,19 +26,36 @@ object Version {
 
   val versionSeparator: Char = '.'
 
-  def comparePerComponent(versions1: List[String], versions2: List[String]): Int = {
-    val versionPairs = versions1.zipAll(versions2, "0", "0")
+  def comparePerComponent(versions1: List[Int], versions2: List[Int]): Int = {
+    val versionPairs = versions1.zipAll(versions2, 0, 0)
     versionPairs
       .map { case (v1, v2) => signum(v1 compareTo v2) }
       .find(_ != 0)
       .getOrElse(0)
   }
 
+  private val componentPattern = "^(\\d+)\\D*".r
+
+  /**
+    * Parse version components from a string like 1.2.3.
+    *
+    * @param str A string containing version components separated by dots.
+    *            Major version component must be a number.
+    *            Minor version components may contain non-numerical entries, only numerical prefix will be taken.
+    *            For example: '1.2.3-u1' -> 1.2.3
+    *            If a component does not start with a number, then set this component to 0.
+    *            For example: '1.2.u1' -> 1.2.0
+    * @return Parsed version object.
+    * @throws IllegalArgumentException Argument is null or empty string.
+    * @throws NumberFormatException Major version component is not a number.
+    */
   def parse(str: String): Version = {
     require(str != null, "String must not be null")
     require(!str.isEmpty, "String must not be empty")
 
     val components = str.split(versionSeparator)
-    Version(components.head, components.tail: _*)
+    val major = components.head.toInt
+    val minors = components.tail.map(c => componentPattern.findFirstMatchIn(c).map(_.group(1).toInt).getOrElse(0))
+    Version(major, minors: _*)
   }
 }
--- src/test/scala/com/micronautics/gitStats/svn/VersionTest.scala
+++ src/test/scala/com/micronautics/gitStats/svn/VersionTest.scala
@@ -6,75 +6,62 @@ import org.scalatest.FunSuite
 
 class VersionTest extends FunSuite {
 
-  test("null major") {
-    intercept[IllegalArgumentException] {
-      Version(null)
-    }
-  }
-
-  test("empty string major") {
-    intercept[IllegalArgumentException] {
-      Version("")
-    }
-  }
-
-
-
   test("compare - 0 and 0") {
-    val v1 = Version("0")
-    val v2 = Version("0")
+    val v1 = Version(0)
+    val v2 = Version(0)
     val res = v1 compare v2
     assert(res === 0, "compare result")
   }
 
   test("compare - 0 and 1") {
-    val v1 = Version("0")
-    val v2 = Version("1")
+    val v1 = Version(0)
+    val v2 = Version(1)
     val res = v1 compare v2
     assert(res === -1, "compare result")
   }
 
   test("compare - 1 and 0") {
-    val v1 = Version("1")
-    val v2 = Version("0")
+    val v1 = Version(1)
+    val v2 = Version(0)
     val res = v1 compare v2
     assert(res === 1, "compare result")
   }
 
   test("compare - 1.1 and 1.0") {
-    val v1 = Version("1", "1")
-    val v2 = Version("1", "0")
+    val v1 = Version(1, 1)
+    val v2 = Version(1, 0)
     val res = v1 compare v2
     assert(res === 1, "compare result")
   }
 
   test("compare - 1.0 and 1.1") {
-    val v1 = Version("1", "0")
-    val v2 = Version("1", "1")
+    val v1 = Version(1, 0)
+    val v2 = Version(1, 1)
     val res = v1 compare v2
     assert(res === -1, "compare result")
   }
 
   test("compare - 1 and 1.0") {
-    val v1 = Version("1")
-    val v2 = Version("1", "0")
+    val v1 = Version(1)
+    val v2 = Version(1, 0)
     val res = v1 compare v2
     assert(res === 0, "compare result")
   }
 
   test("compare - 1.0 and 1") {
-    val v1 = Version("1", "0")
-    val v2 = Version("1")
+    val v1 = Version(1, 0)
+    val v2 = Version(1)
     val res = v1 compare v2
     assert(res === 0, "compare result")
   }
 
-  test("compare - 1.2.3-u1 and 1.2.3") {
-    val v1 = Version("1", "2", "3-u1")
-    val v2 = Version("1", "2", "3")
+  test("compare - 1.9.5 and 1.10") {
+    val v1 = Version(1, 9, 5)
+    val v2 = Version(1, 10)
     val res = v1 compare v2
-    assert(res === 1, "compare result")
+    assert(res === -1, "compare result")
   }
+
 
 
   test("parse - null") {
@@ -89,18 +76,29 @@ class VersionTest extends FunSuite {
     }
   }
 
+  test("parse - major is not a number") {
+    intercept[NumberFormatException] {
+      parse("1v.2.3")
+    }
+  }
+
   test("parse - 1") {
     val res = parse("1")
-    assert(res === Version("1"), "version")
+    assert(res === Version(1), "version")
   }
 
   test("parse - 1.0") {
     val res = parse("1.0")
-    assert(res === Version("1", "0"), "version")
+    assert(res === Version(1, 0), "version")
   }
 
   test("parse - 1.2.3-u1") {
     val res = parse("1.2.3-u1")
-    assert(res === Version("1", "2", "3-u1"), "version")
+    assert(res === Version(1, 2, 3), "version")
+  }
+
+  test("parse - 1.2.u1") {
+    val res = parse("1.2.u1")
+    assert(res === Version(1, 2, 0), "version")
   }
 }
