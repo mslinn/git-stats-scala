3518a8323eba6cc1282968fab03b2de34650fd1f
Arseniy Tashoyan <tashoyan@gmail.com>
2017-09-04 00:41:47 +0300

    Smoke test

--- src/main/scala/com/micronautics/gitStats/svn/SvnStats.scala
+++ src/main/scala/com/micronautics/gitStats/svn/SvnStats.scala
@@ -1,14 +1,28 @@
 package com.micronautics.gitStats.svn
 
-import com.micronautics.gitStats.ConfigGitStats
+import com.micronautics.gitStats.{ConfigGitStats, Version}
 
 //TODO Embed into th main app, remove main method from here
 object SvnStats extends App {
 
   implicit val config: ConfigGitStats = ConfigGitStats()
   val svnCmd: SvnCmd = new SvnCmd()
-  println(s"Subversion command version: ${svnCmd.svnVersion}")
+  if (svnCmd.svnVersion.isEmpty) {
+    println("Failed to determine Subversion command version; exiting")
+  }
+  val svnVersion: Version = svnCmd.svnVersion.get
+  println(s"Subversion command version: $svnVersion")
+
+  val svnMinimalSupportedVersion = Version.parse("1.7")
+  if (svnVersion < svnMinimalSupportedVersion) {
+    println(s"Unsupported Subversion version: $svnVersion; $svnMinimalSupportedVersion or higher is required")
+    sys.exit(-1)
+  }
 
   val svnUsers: SvnUsers = new SvnUsers()
   println(s"Subversion user names: ${svnUsers.userNames}")
+  if (svnUsers.userNames.isEmpty) {
+    println("Failed to detect Subversion users; exiting")
+    sys.exit(-1)
+  }
 }
