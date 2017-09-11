ce577953ba5d36825d693743a4cacdbad8d494fd
Arseniy Tashoyan <tashoyan@gmail.com>
2017-09-11 14:52:19 +0300

    Temporarily add config for tests

--- src/main/scala/com/micronautics/gitStats/git/GitStats.scala
+++ src/main/scala/com/micronautics/gitStats/git/GitStats.scala
@@ -2,13 +2,26 @@ package com.micronautics.gitStats.git
 
 import java.nio.file.Path
 
-import com.micronautics.gitStats.{AggCommit, ConfigGitStats}
+import com.micronautics.gitStats.AggCommit.AggCommits
+import com.micronautics.gitStats.ConfigGitStats
+import com.micronautics.gitStats.ProjectDir._
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object GitStats {
 
-  def commits(scmProjectDirs: Iterable[Path])(implicit config: ConfigGitStats): Iterable[(Path, Try[AggCommit])] = {
-    ???
+  def commits(scmProjectDirs: Iterable[Path])(implicit config: ConfigGitStats): Iterable[(Path, Try[AggCommits])] = {
+    val gitProjectDirs = scmProjectDirs.filter(_.isGitRepo)
+    if (config.verbose)
+      println(gitProjectDirs.mkString("Detected Git repository directories:\n", "\n", "\n"))
+
+    val gitRepoDirs = gitProjectDirs.map(new GitRepoDir(_))
+    gitRepoDirs.map { repoDir =>
+      try {
+        repoDir.dir -> Success(repoDir.aggCommits)
+      } catch {
+        case e: Throwable => repoDir.dir -> Failure(e)
+      }
+    }
   }
 }
